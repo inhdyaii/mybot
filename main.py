3ef5ebce36f66a2ab5b91aa36d784a1188d6db9d
inhdyaii <69579471+inhdyaii@users.noreply.github.com>
2025-03-23 14:09:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+
+from flask import Flask
+from threading import Thread
+from highrise.__main__ import *
+import time
+
+
+class WebServer():
+
+  def __init__(self):
+    self.app = Flask(__name__)
+
+    @self.app.route('/')
+    def index() -> str:
+      return "Alive"
+
+  def run(self) -> None:
+    self.app.run(host='0.0.0.0', port=8080)
+
+  def keep_alive(self):
+    t = Thread(target=self.run)
+    t.start()
+
+
+class RunBot():
+  room_id = "67d1671ed61ff74e95ff36c4"
+  bot_token = "3c2f6c86be7c7b1b75419251d1040dcc5606128481a6482866b6b22dae74f154"
+  bot_file = "x2"
+  bot_class = "Bot"
+
+  def __init__(self) -> None:
+    self.definitions = [
+        BotDefinition(
+            getattr(import_module(self.bot_file), self.bot_class)(),
+            self.room_id, self.bot_token)
+    ]  # More BotDefinition classes can be added to the definitions list
+
+  def run_loop(self) -> None:
+    while True:
+      try:
+        arun(main(self.definitions))
+
+      except Exception as e:
+        # Print the full traceback for the exception
+        import traceback
+        print("Caught an exception:")
+        traceback.print_exc()  # This will print the full traceback
+        time.sleep(1)
+        continue
+
+
+if __name__ == "__main__":
+  WebServer().keep_alive()
+
+  RunBot().run_loop()
